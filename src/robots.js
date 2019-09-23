c2dbb2490a9d2350007d0af4f3de49c3582443ae
Nagham Ramadan <nagham.ramadan@undp-pogar.local>
2019-09-23 18:05:22 +0300

    first commit

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+{
+    id: 1,
+    name: 'Leanne Gragam',
+    username: 'Bret',
+    email: 'sincere@april.com'
+},
+{
+    id: 2,
+    name: 'Leanne Gragam',
+    username: 'Bret',
+    email: 'sincere@april.com'
+},
+{
+    id: 3,
+    name: 'Leanne Gragam',
+    username: 'Bret',
+    email: 'sincere@april.com'
+},
+{
+    id: 4,
+    name: 'Leanne Gragam',
+    username: 'Bret',
+    email: 'sincere@april.com'
+},
+{
+    id: 5,
+    name: 'Leanne Gragam',
+    username: 'Bret',
+    email: 'sincere@april.com'
+},
+]
